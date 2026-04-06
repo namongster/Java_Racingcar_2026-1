5c00ba292b41d68037a90e005020c9222922acb4
namhong <soccernh08@gmail.com>
2026-04-06 14:07:54 +0900

    경주 게임 출력 함수

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.*;
-import org.assertj.core.util.VisibleForTesting;
 
 import java.util.Arrays;
 
 public class Application {
     static String[] cars;
+    static int[] count;
     class player{
         String name;
         int count;
@@ -42,17 +42,10 @@ public class Application {
         }
         return game;
     }
-    public static void main(String[] args) {
-        //이름 입력
-        name();
-        //게임 횟수 입력
-        int game = playtime();
-
-        //경주 게임 출력
-
+    public static void play(int game){
         String[] race = new String[cars.length];
         Arrays.fill(race,"");
-        int[] count = new int[cars.length];
+        count = new int[cars.length];
         for(int i =0;i<game;i++){
             for (int j = 0; j < cars.length; j++) {
                 int num = Randoms.pickNumberInRange(0, 9);
@@ -67,7 +60,14 @@ public class Application {
             }
             System.out.println();
         }
-
+    }
+    public static void main(String[] args) {
+        //이름 입력
+        name();
+        //게임 횟수 입력
+        int game = playtime();
+        //경주 게임 출력
+        play(game);
         //우승자 출력
         System.out.print("최종 우승자 : ");
         int s = cars.length;
